2673d9956d199187c56ebb1e57556c5200340750
longyuanbing <longyuanbing@lifesense.com>
2017-09-13 00:27:06 +0800

    eureka

--- src/main/java/com/yuandu/TestApp.java
+++ src/main/java/com/yuandu/TestApp.java
@@ -1,10 +1,12 @@
 package com.yuandu;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages="com.yuandu")
 public class TestApp {
     public static void main(String[] args){
         SpringApplication.run(TestApp.class, args);
